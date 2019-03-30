e0ad157438dc3a6ed15a611fe22ac5723e82eec1
Evgeni Sviridenko <paratagas@gmail.com>
2019-03-30 20:26:03 +0300

    Set default backend data

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
-
 const app = express();
+const initialData = require('./default-data.json');
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+// app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/', (request, response, next) => {
+  response.json(initialData);
+});
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
